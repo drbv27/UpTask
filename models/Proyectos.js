72471d49dbc0bae934b6b6cb9ddb1e5eda48df02
Diego <drbv27@gmail.com>
2021-10-26 15:33:02 -0500

    Unique URL

--- models/Proyectos.js
+++ models/Proyectos.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const db = require("../config/db");
-
 const slug = require("slug");
+const shortid = require("shortid");
 
 const Proyectos = db.define(
   "proyectos",
@@ -20,7 +20,7 @@ const Proyectos = db.define(
       beforeCreate(proyecto) {
         const url = slug(proyecto.nombre).toLowerCase();
 
-        proyecto.url = url;
+        proyecto.url = `${url}-${shortid.generate()}`;
       },
     },
   }
