f079ea31adb4a4327f7c021a35d0ae89f91fbfbf
Diego <drbv27@gmail.com>
2021-10-25 20:13:09 -0500

    Form

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -3,3 +3,31 @@ exports.proyectosHome = (req, res) => {
     nombrePagina: "Proyectos",
   }); //es el pug
 };
+
+exports.formularioProyecto = (req, res) => {
+  res.render("nuevoProyecto", {
+    nombrePagina: "Nuevo Proyecto",
+  });
+};
+
+exports.nuevoProyecto = (req, res) => {
+  //Enviar a la consola lo que el usuario escriba
+  /* console.log(req.body); */
+  //validar que si tengamos algo en el input
+  const { nombre } = req.body;
+  let errores = [];
+
+  if (!nombre) {
+    errores.push({ texto: "Agrega un nombre al proyecto" });
+  }
+  //si hay errores
+  if (errores.length > 0) {
+    res.render("nuevoProyecto", {
+      nombrePagina: "Nuevo Proyecto",
+      errores,
+    });
+  } else {
+    //no hay errores
+    //Insertar en la DB.
+  }
+};
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const routes = require("./routes");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 //crear una app de express
 const app = express();
@@ -14,6 +15,9 @@ app.use(express.static("public"));
 app.set("view engine", "pug");
 //Añadir la carpeta de vistas
 app.set("views", path.join(__dirname, "./views"));
+
+//Habilitar body parser para leer datos del formulario
+app.use(bodyParser.urlencoded({ extend: true }));
 
 app.use("/", routes());
 
