c7f7f00460d729e25a3e3a5b816c0f65ee4e008c
Diego <drbv27@gmail.com>
2021-10-27 15:17:03 -0500

    adding helpers

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,18 +1,24 @@
 const Proyectos = require("../models/Proyectos");
 
-exports.proyectosHome = (req, res) => {
+exports.proyectosHome = async (req, res) => {
+  const proyectos = await Proyectos.findAll();
+  //es lo mismo que SELECT * from proyectos
   res.render("index", {
-    nombrePagina: "Proyectos",
+    nombrePagina: "Proyectos " /* + res.locals.year */,
+    proyectos,
   }); //es el pug
 };
 
-exports.formularioProyecto = (req, res) => {
+exports.formularioProyecto = async (req, res) => {
+  const proyectos = await Proyectos.findAll();
   res.render("nuevoProyecto", {
     nombrePagina: "Nuevo Proyecto",
+    proyectos,
   });
 };
 
 exports.nuevoProyecto = async (req, res) => {
+  const proyectos = await Proyectos.findAll();
   //Enviar a la consola lo que el usuario escriba
   /* console.log(req.body); */
   //validar que si tengamos algo en el input
@@ -27,6 +33,7 @@ exports.nuevoProyecto = async (req, res) => {
     res.render("nuevoProyecto", {
       nombrePagina: "Nuevo Proyecto",
       errores,
+      proyectos,
     });
   } else {
     //no hay errores
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require("express");
 const routes = require("./routes");
 const path = require("path");
 const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+
+//helpers con algunas funciones
+const helpers = require("./helpers");
 
 //Crear la conexion a la DB
 const db = require("./config/db");
@@ -25,6 +29,20 @@ app.use(express.static("public"));
 app.set("view engine", "pug");
 //Añadir la carpeta de vistas
 app.set("views", path.join(__dirname, "./views"));
+
+//Pasar var dump a la aplicacion
+app.use((req, res, next) => {
+  res.locals.vardump = helpers.vardump;
+  next();
+});
+
+//aprendiendo middleware
+/* app.use((req, res, next) => {
+  const fecha = new Date();
+  res.locals.year = fecha.getFullYear();
+  console.log("yo soy middleware");
+  next();
+}); */
 
 //Habilitar body parser para leer datos del formulario
 app.use(bodyParser.urlencoded({ extend: true }));
